6c4800940a5aadcaa1e50ebb9fb30fd8d186189d
Anirban Chakraborty <anirbanchakraborty@tamu.edu>
2019-09-05 14:59:11 -0500

    Vectorizing computeT_for4 function

--- sep5class.R
+++ sep5class.R
@@ -30,7 +30,7 @@ computeT_for2 <- function(X,group)
 
 
 library(microbenchmark)
-microbenchmark(computeT_for, computeT_for2)
+microbenchmark(computeT_for3, computeT_for2)
 
 
 
@@ -49,4 +49,20 @@ computeT_for3 <- function(X, group)
     var2 = var(X[group == 2,j])
    Tstats[j] = (m1 - m2) / sqrt( var/n1 + var2/n2)
   }
+}
+
+computeT_for4 <- function(X, group)
+{
+  n1 = sum(group == 1)
+  n2 = sum(group == 2)
+  p = ncol(X)
+  Tstats = rep(0,p)
+  m1 = colMeans(X[group =1, ])
+  m2 = colMeans(X[group =2, ])
+  m1matrixed <- matrix(m1, n1, p, byrow = TRUE)
+  m2matrixed <- matrix(m2, n2, p, byrow = TRUE)
+  var1 <- colSums((X[group =1, ] - m1matrixed )^2)/(n1 - 1)
+  var2 <- colSums((X[group =2, ] - m2matrixed )^2)/(n2 - 1)
+                                                    
+  Tstats = (m1 - m2) / sqrt(var1/n1 + var2/n2)
 }
