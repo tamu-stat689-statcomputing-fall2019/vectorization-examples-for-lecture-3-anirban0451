5f0165770f5ac2b11410e21b93ba363c951c2680
Anirban Chakraborty <anirbanchakraborty@tamu.edu>
2019-09-05 14:33:41 -0500

    This commit will funish the computeT_for function for the t-test

---
+++ sep5class.R
@@ -0,0 +1,17 @@
+p <- 1000
+n <- 50
+set.seed(03875)
+X <- matrix(nrom(n*p, mean = 10, sd = 3), n, p)
+group <- rep(1:2, each = n/2)
+
+
+
+computeT_for <- function(X, group)
+{
+  Tstats <- rep(0,p)
+  for(j in 1:p)
+  {
+    Tstats[j] <- t.test(X[,j] ~ group)$stat
+  }
+  return(Tstats)
+}
