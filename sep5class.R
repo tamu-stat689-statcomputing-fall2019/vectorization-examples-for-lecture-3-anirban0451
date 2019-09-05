a48bc7dc33d704803407a62bf41472d366bf373b
Anirban Chakraborty <anirbanchakraborty@tamu.edu>
2019-09-05 14:41:33 -0500

    Finished computeT_for3 function

--- sep5class.R
+++ sep5class.R
@@ -16,6 +16,8 @@ computeT_for <- function(X, group)
   return(Tstats)
 }
 
+
+
 computeT_for2 <- function(X,group)
 {
   Tstats <- rep(0,p)
@@ -24,4 +26,27 @@ computeT_for2 <- function(X,group)
     Tstat[j] <- t.test(X[group == 1,j],X[group == 2,j])$stat
   }
   return(Tstats)
+}
+
+
+library(microbenchmark)
+microbenchmark(computeT_for, computeT_for2)
+
+
+
+
+
+computeT_for3 <- function(X, group)
+{
+  n1 = sum(group == 1)
+  n2 = sum(group == 2)
+  Tstats = rep(0,p)
+  for (j in 1:p)
+  {
+    m1 = mean(X[group == 1,j])
+    m2 = mean(X[group == 2,j])
+    var1 = var(X[group == 1,j])
+    var2 = var(X[group == 2,j])
+   Tstats[j] = (m1 - m2) / sqrt( var/n1 + var2/n2)
+  }
 }
